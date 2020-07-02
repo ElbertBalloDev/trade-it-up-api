60b65a789f48eef6497826aa76d7d7bfd56069de
Elbert Ballo <eballofullstackdev@gmail.com>
2020-07-01 21:57:04 -0700

    client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Viewport, Theme } from './components/UI';
+
+import './App.css';
+
+function App() {
+  return (
+    <Theme>
+      <Viewport>
+        <h1>Trade ins</h1>
+      </Viewport>
+    </Theme>
+  );
+}
+
+export default App;
---
+++ client/src/components/UI/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    powderWhite: '#FFFDF9',
+    persianGreen: '#06B49A',
+    lightBlue: '#AFDBD2',
+    onyx: '#36313D'
+  },
+  fonts: ['sans-serif', 'Roboto'],
+  fontSizes: {
+    small: '1em',
+    medium: '2em',
+
+    large: '3em'
+  }
+};
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const Viewport = styled.div`
+  max-width: 1300px;
+  width: auto;
+  height: auto;
+  margin: 0 auto;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-director: row;
+  flex-wrap: wrap;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-director: column;
+  flex-wrap: wrap;
+`;
+
+export const Input = styled.input``;
+
+export const Theme: React.FC<IProps> = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
