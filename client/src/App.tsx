a88456bde09c9c67a61d7380e3e918faf8312d01
Gilbert <gilwong3105@gmail.com>
2020-07-02 21:45:37 -0700

    Add some more initial styled components for future app usage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Viewport, Theme } from './components/UI';
+import { Viewport, Theme, Row, RowItem } from './components/UI';
 
 import './App.css';
 
@@ -8,6 +8,14 @@ function App() {
     <Theme>
       <Viewport>
         <h1>Trade ins</h1>
+				<Row>
+					<RowItem>Item 1</RowItem>
+					<RowItem>Item 2</RowItem>
+					<RowItem>Item 3</RowItem>
+					<RowItem>Item 4</RowItem>
+					<RowItem>Item 5</RowItem>
+					<RowItem>Item 6</RowItem>
+				</Row>
       </Viewport>
     </Theme>
   );
--- client/src/components/UI/index.tsx
+++ client/src/components/UI/index.tsx
@@ -30,8 +30,16 @@ export const Viewport = styled.div`
 export const Row = styled.div`
   display: flex;
   flex-director: row;
-  flex-wrap: wrap;
+	flex-wrap: wrap;
 `;
+
+export const RowItem = styled.div`
+	flex: 1 1 30%;
+	padding: 10px;
+	height: 200px;
+	border: 1px solid black;
+	margin: 10px;
+`
 
 export const Column = styled.div`
   display: flex;
